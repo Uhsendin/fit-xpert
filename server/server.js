42d7b2d622346531b01be52261098b34facdc784
Brandon Rodriguez <chilebr99@gmail.com>
2023-07-24 14:40:28 -0400

    intergrated mongoDB

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/database.js';
 const port = process.env.PORT || 8080;
 import userRoutes from './routes/userRoutes.js';
 
+connectDB();
 const app = express();
 
 app.use('/api/users', userRoutes);
