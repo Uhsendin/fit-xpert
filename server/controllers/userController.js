88fa7ee65cd6cd9f5dcf2b5c8922a2ed567178b5
Brandon Rodriguez <chilebr99@gmail.com>
2023-07-24 14:19:11 -0400

    Added api routes for users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,4 +6,38 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Auth User' });
 });
 
-export { authUser };
+// Desc     Register a new user
+// route    POST /api/users
+// access   Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register User' });
+});
+
+// Desc     Logout user
+// route    POST /api/users/logout
+// access   Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout User' });
+});
+
+// Desc     Get user profile
+// route    GET /api/users/profile
+// access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User profile' });
+});
+
+// Desc     Update user profile
+// route    PUT /api/users/profile
+// access   Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user profile' });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
