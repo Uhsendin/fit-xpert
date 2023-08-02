3aca7d4a55bc8726f474e8d911c49755f53e3923
Brandon Rodriguez <chilebr99@gmail.com>
2023-08-02 19:34:24 -0400

    redux toolkit setup for state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import store from './store.js';
+import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen.jsx';
 import { AuthenticationForm } from './screens/AuthenticationScreen.jsx';
 import DashboardScreen from './screens/DashboardScreen.jsx';
@@ -22,7 +24,10 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+    ,
+  </Provider>,
 );
