837d9ed6bf7e7b0e67b7438ca68a1c7e46b10b7d
Brandon Rodriguez <chilebr99@gmail.com>
2023-08-02 16:01:43 -0400

    Added Toast container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MantineProvider } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
@@ -9,6 +11,7 @@ const App = () => {
         withNormalizeCSS
         theme={{ primaryColor: 'pink' }}
       >
+        <ToastContainer />
         <Outlet />
       </MantineProvider>
     </>
