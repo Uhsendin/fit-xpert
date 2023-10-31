2867b01802667887531c610bc9d024dc19c5ad36
Brandon Rodriguez <chilebr99@gmail.com>
2023-10-31 16:04:04 -0400

    created FoodModel schema

---
+++ server/models/foodModel.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+const foodSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    foodId: {
+      type: Number,
+      required: true,
+    },
+    foodName: {
+      type: String,
+      required: true,
+    },
+    portionSize: {
+      type: String,
+      required: true,
+    },
+    servingSize: {
+      type: Number,
+      required: true,
+    },
+    nutrients: {
+      type: {
+        kcal: {
+          type: Number,
+          required: true,
+        },
+        protein: {
+          type: Number,
+          required: true,
+        },
+        carbs: {
+          type: Number,
+          required: true,
+        },
+        fat: {
+          type: Number,
+          required: true,
+        },
+      },
+
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Food = mongoose.model('Food', foodSchema);
+
+export default Food;
