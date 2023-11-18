b1cf72270db28f2c2dd37975e335a9b7d6282b2b
Brandon Rodriguez <chilebr99@gmail.com>
2023-11-18 18:27:05 -0500

    Added route for fetching foods by date

--- server/controllers/foodController.js
+++ server/controllers/foodController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Food from '../models/foodModel.js';
 
 // Desc     Create a new food
-// route    POST /api/foods
+// route    POST /api/foods/
 // access   Private
 const createFood = asyncHandler(async (req, res) => {
   const userId = req.user._id;
@@ -36,4 +36,24 @@ const createFood = asyncHandler(async (req, res) => {
   res.json(savedFood);
 });
 
-export { createFood };
+// Desc     Get all foods baseed on date range
+// route    POST /api/foods/:date
+// access   Private
+const getFoodByDate = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const providedTimestamp = parseInt(req.params.date);
+  const twentyFourHoursAgo = providedTimestamp - 24 * 60 * 60 * 1000;
+
+  const foods = await Food.find({
+    createdAt: { $gte: twentyFourHoursAgo, $lt: providedTimestamp },
+    user: userId,
+  });
+
+  if (foods === null || foods.length === 0) {
+    res.status(404);
+    throw new Error('Foods not found');
+  }
+  res.json(foods);
+});
+
+export { createFood, getFoodByDate };
--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleWare.js';
-import { createFood } from '../controllers/foodController.js';
+import { createFood, getFoodByDate } from '../controllers/foodController.js';
 
 router.post('/foods', protect, createFood);
+router.get('/foods/:date', protect, getFoodByDate);
 export default router;
