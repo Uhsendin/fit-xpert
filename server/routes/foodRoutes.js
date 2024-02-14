b574d909d83b44c8044621fc77564fb39556a7b1
Brandon Rodriguez <chilebr99@gmail.com>
2024-02-14 15:28:55 -0500

    Added delete to server route

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import { protect } from '../middleware/authMiddleWare.js';
 import {
   createFood,
+  deleteFoodById,
   getFoodByDate,
   updateFoodById,
 } from '../controllers/foodController.js';
@@ -10,4 +11,5 @@ import {
 router.post('/foods', protect, createFood);
 router.get('/foods/:date', protect, getFoodByDate);
 router.put('/foods/:id', protect, updateFoodById);
+router.delete('/foods/:id', protect, deleteFoodById);
 export default router;
