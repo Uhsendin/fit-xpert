27b946ded2b5fa76bcb1ad2558b3c2134cb12d5f
Brandon Rodriguez <chilebr99@gmail.com>
2024-07-11 23:00:53 -0400

    depolyment changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/database.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 const port = process.env.PORT || 8080;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 import userRoutes from './routes/userRoutes.js';
 import noteRoutes from './routes/noteRoutes.js';
 import foodRoutes from './routes/foodRoutes.js';
@@ -19,6 +23,12 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api', noteRoutes);
 app.use('/api', foodRoutes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '../client/dist')))
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '../client/dist', 'index.html'))
+  })
+}
 app.get('/', (req, res) => res.send('Server is ready'));
 
 app.use(notFound);
